69bb44400bb12721bb9098bfec348a71a85cca3d
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2020-09-30 17:53:35 +0200

    Implement simple logging

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-let mymap = L.map("mapid").setView([51.505, -0.09], 13);
+let mymap = L.map("mapid").setView([49.8217096, 19.054798], 15);
+let activeLocation = null;
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
   {
     maxZoom: 18,
     attribution:
-    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
   }
 ).addTo(mymap);
+
+function showModal() {
+  let modal = document.querySelector(".modal");
+  modal.className = "modal active";
+}
+
+function hideModal() {
+  let modal = document.querySelector(".modal");
+  modal.className = "modal";
+
+  document.querySelector('input[name=reason]').value = "";
+}
+
+function markPlace(reason) {
+  let marker = L.marker([activeLocation.lat, activeLocation.lng]).addTo(mymap);
+  marker.bindPopup(`<span class="popup">${reason}</span>`).openPopup();
+}
+
+function handleFormSubmit(e) {
+  let reason = e.target.querySelector('input').value;
+
+  e.preventDefault()
+  markPlace(reason)
+  hideModal()
+}
+
+let popup = L.popup();
+
+function onMapClick(e) {
+  activeLocation = e.latlng
+  showModal();
+}
+
+mymap.on("click", onMapClick);
+
+document.querySelector(".modal__close").addEventListener("click", hideModal);
+document.querySelector("form").addEventListener("submit", handleFormSubmit);
